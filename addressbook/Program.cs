e94d4b25ed457c3ce333abfc6fa4d03a5ae9dfa7
sahithi <sahithipari99@gmail.com>
2021-07-03 21:42:39 +0530

    [sahithi] address book1

---
+++ addressbook/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace addressbook
+{
+    using System;
+
+    namespace Address_Book_System
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine(" Welcome to Address Book Program");
+            }
+        }
+    }
+}
---
+++ addressbook/contact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace addressbook
+{
+    class contact
+    {
+            public string FirstName;
+            public string LastName;
+            public string Address;
+            public string City;
+            public string State;
+            public int Zip;
+            public long PhoneNumber;
+            public string Email;
+        }
+}
