439443ab65b0969ef59664d6ae34fa0cb17c14e4
sahithi <sahithipari99@gmail.com>
2021-07-04 12:34:16 +0530

    [sahithi] address book4

--- addressbook/AddressBook.cs
+++ addressbook/AddressBook.cs
@@ -4,22 +4,33 @@ using System.Text;
 
 namespace AddressBookSystem
 {
-    class AddressBook
-    {
+    
+    
 
-        // List for (Contact) class objects is created.....
-        public List<Contact> ContactList;
-        public AddressBook()
+        class AddressBook
         {
-            this.ContactList = new List<Contact>();
-        }
-        public void Add_Contacts(Contact contact)
-        {
-            // add class objects in list c.....
-            this.ContactList.Add(contact);
+
+            // List for (Contact) class objects is created.....
+            public List<Contact> ContactList;
+            public AddressBook()
+            {
+                this.ContactList = new List<Contact>();
+            }
+            public void Add_Contacts(Contact contact)
+            {
+                // add class objects in list c.....
+                this.ContactList.Add(contact);
+            }
+            public int FindByPhoneNumber(int number)
+            {
+                // Returns the index of Contact....
+                return this.ContactList.FindIndex(contact => contact.PhoneNumber.Equals(number));
+
+            }
+
         }
     }
-}
+
 
 
 
--- addressbook/Program.cs
+++ addressbook/Program.cs
@@ -2,35 +2,67 @@
 
 namespace AddressBookSystem
 {
+   
+        }
+namespace AddressBookSystem
+{
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" Welcome to Address Book Program \n");
+            Console.WriteLine(" Welcome to Address Book Program ");
 
             // Object of AdressBook is Created.....
             AddressBook addressBook = new AddressBook();
-
-            // Obtions are Show to User.......
-            Console.WriteLine(" Options");
-            Console.WriteLine(" 1. Add a new Contact");
-
-            // Reads the Option.....
-            Console.Write("\n Select Options : ");
-            int option = Convert.ToInt32(Console.ReadLine());
-
-            switch (option)
+            bool stop = false;
+            while (!stop)
             {
-                case 1:
-                    Contact contact = new Contact();   // New Contact Object is Created ....
-                    Read_Contact(contact);             //method is called for input of contact details....
-                    addressBook.Add_Contacts(contact);  // contact details is added to a List...
-                    break;
-                default:
-                    break;
+                // Obtions are Show to User.......
+                Console.WriteLine("\n Options");
+                Console.WriteLine(" 1. Add a new Contact");
+                Console.WriteLine(" 2.Edit Contact ");
+                Console.WriteLine(" 3.Number of Contacts");
+                Console.WriteLine(" 0.Exit ");
+
+                // Reads the Option.....
+                Console.Write("\n Select Options : ");
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        Contact contact = new Contact();   // New Contact Object is Created ....
+                        Read_Contact(contact);             //method is called for input of contact details....
+                        addressBook.Add_Contacts(contact);  // contact details is added to a List...
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter the Phone Number of Contact you wish to Edit");
+                        long phoneNumber = long.Parse(Console.ReadLine());
+                        int index = addressBook.FindByPhoneNumber((int)phoneNumber);
+                        if (index == -1)
+                        {
+                            Console.WriteLine("\n No Contact Exists With Following Phone Number\n");
+                        }
+                        else
+                        {
+                            Contact contact2 = new Contact();
+                            Read_Contact(contact2);
+                            addressBook.ContactList[index] = contact2;
+                            Console.WriteLine("Contact Updated Successfully");
+                        }
+                        break;
+                    case 0:
+                        stop = true;
+                        break;
+                    case 3:
+                        // Number of Contacts is give......(count)...
+                        Console.WriteLine("\n Number of Contacts : " + addressBook.ContactList.Count);
+                        break;
+                    default:
+                        break;
+                }
             }
-            // Number of Contacts is give......(count)...
-            Console.WriteLine("\n Number of Contacts : " + addressBook.ContactList.Count);
+
         }
 
         public static void Read_Contact(Contact contact)
@@ -55,6 +87,7 @@ namespace AddressBookSystem
         }
     }
 }
+
 
 
 
